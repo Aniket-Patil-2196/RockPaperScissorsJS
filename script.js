aa5e7065225a857a7fa9dfff5df1661afa31d470
Aniket-Patil-2196 <aniketjaysingpatil2006@gmail.com>
2025-05-04 22:22:10 +0530

    add initial js

---
+++ script.js
@@ -0,0 +1,44 @@
+let userScore = 0;
+let computerScore = 0;
+
+const choices = document.querySelectorAll('.choice');
+const playerScore = document.querySelector('.player-score');
+const compScore = document.querySelector('.computer-score');
+
+const comChoice = () => {
+    const choices = ['rock', 'paper', 'scissors'];
+    const idx = Math.floor(Math.random() * choices.length);
+    return choices[idx];
+}
+
+const playGame = (userChoice) => {
+    const compChoice = comChoice();
+    console.log("userChoice = " + userChoice);
+    console.log("compChoice = " + compChoice);
+    checkWinner(userChoice, compChoice);
+    playerScore.innerText = userScore;
+    compScore.innerText = computerScore;
+}
+
+
+choices.forEach(choice => {
+    choice.addEventListener('click', () => {
+        console.log("clicked");
+        const userChoice = choice.getAttribute('id');
+        playGame(userChoice);
+    });
+});
+
+const checkWinner = (userChoice, compChoice) => {
+    if (userChoice === compChoice) {
+        console.log("It's a draw!");
+    } else if ((userChoice === 'rock' && compChoice === 'scissors') ||
+               (userChoice === 'paper' && compChoice === 'rock') ||
+               (userChoice === 'scissors' && compChoice === 'paper')) {
+        userScore++;
+        console.log("You win! Your score: " + userScore);
+    } else {
+        computerScore++;
+        console.log("You lose! Computer score: " + computerScore);
+    }
+}
